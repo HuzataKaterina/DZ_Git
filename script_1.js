00cb254bc3cb75da18626eae427051ece412b6a6
Huzata Katerina <guzatayaes@gmail.com>
2023-11-29 12:00:15 +0200

    add function printName

--- script_1.js
+++ script_1.js
@@ -4,3 +4,8 @@ const sayHello = () => {
     console.log(`Hello`);
 };
 sayHello();
+
+const printName = () => {
+    let name = prompt(`enter your name`);
+    {return console.log(`Name user: ${name}`)}
+};
